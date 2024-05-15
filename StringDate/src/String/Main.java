3ad35dbc29e59dc8fd7f4257bbc670321da8fef5
ZsoltWebler <128636694+ZsoltWebler@users.noreply.github.com>
2024-05-15 17:35:10 +0200

    Init

---
+++ StringDate/src/String/Main.java
@@ -0,0 +1,13 @@
+package String;
+
+import java.util.StringJoiner;
+
+public class Main {
+    public static void main(String[] args) {
+        isPalindrome("alma");
+    }
+
+    public static boolean isPalindrome(String s){
+        return false;
+    }
+}
