89539df48da6cf26459b6868627b2ea6860eaef4
Róbert Nagy <roister@windowslive.com>
2024-05-15 18:46:56 +0200

    feat: add 05.15exercise

--- StringDate/src/String/Main.java
+++ StringDate/src/String/Main.java
@@ -4,10 +4,39 @@ import java.util.StringJoiner;
 
 public class Main {
     public static void main(String[] args) {
-        isPalindrome("alma");
+        System.out.println(isPalindrome("amama"));
+        occurences("almakortealmabananalmasajt", "alma");
+        reverseString("alma");
     }
 
-    public static boolean isPalindrome(String s){
+    public static boolean isPalindrome(String s) {
+        String palindrome = "";
+        int length = s.length();
+        for (int i = length - 1; i >= 0; i--) {
+            palindrome += s.charAt(i);
+        }
+        if (palindrome.equals(s)) {
+            return true;
+        }
         return false;
+
+    }
+
+    public static void occurences(String s, String subString) {
+        int count = 0;
+        for (int i = 0; i < s.length() - 2 - subString.length(); i++) {
+            if(s.substring(i,i+subString.length()).equals(subString)){
+                count +=1;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static String reverseString(String s){
+        String reverse = "";
+        for(int i = s.length()-1; i>=0;i--){
+            reverse += s.charAt(i);
+        }
+        return reverse;
     }
 }
