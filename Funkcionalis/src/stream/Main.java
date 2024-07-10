64aec23ce70471fa13425fa6b1ebe448506741f9
ZsoltWebler <128636694+ZsoltWebler@users.noreply.github.com>
2024-07-10 17:39:29 +0200

    Init

--- Funkcionalis/src/stream/Main.java
+++ Funkcionalis/src/stream/Main.java
@@ -1,5 +1,7 @@
 package stream;
 
+import practice.StringRule;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -10,11 +12,13 @@ public class Main {
     public static void main(String[] args) {
 
 
-        List<Integer> numbers=Arrays.asList(1,2,3,4,5,6,7,8,9);
-        Map<Boolean, List<Integer>>evenOddMap=numbers.stream()
-                .collect(Collectors.partitioningBy(n->n%2==0));
+    }
 
-        System.out.println();
+    public static void applyRule(StringRule rule, String str){
+
+
+        String isEmpty = rule.apply(str) ? "" : "nem ";
+        System.out.println("A kapott string "+isEmpty+"üres!" );
 
     }
 }
