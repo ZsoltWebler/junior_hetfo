f25851e8d0bb9dc8dac334b5690a170c7ba84f2b
ZsoltWebler <128636694+ZsoltWebler@users.noreply.github.com>
2024-09-11 18:29:56 +0200

    Init

--- Funkcionalis/src/tdd/Solution.java
+++ Funkcionalis/src/tdd/Solution.java
@@ -3,9 +3,35 @@ package tdd;
 class Solution {
 
 
-
     public int lengthOfLongestSubstring(String s) {
-        throw new UnsupportedOperationException();
+
+
+        String appearedCharacters = "";
+        int longestSubStringCount = 0;
+        int counter = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char currentChar = s.charAt(i);
+
+            if (!appearedCharacters.contains(String.valueOf(currentChar))) {
+                appearedCharacters += currentChar;
+                counter++;
+            } else {
+                appearedCharacters = currentChar + "";
+                longestSubStringCount = Math.max(longestSubStringCount, counter);
+                counter = 1;
+            }
+
+        }
+
+        return longestSubStringCount;
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+
+        return 0;
     }
 
 }
--- Funkcionalis/src/tdd/SolutionTest.java
+++ Funkcionalis/src/tdd/SolutionTest.java
@@ -58,5 +58,70 @@ public class SolutionTest {
         assertEquals(expected, actualValue);
     }
 
+    @Test
+    public void findMedianSortedArraysTest_1() {
+        //Given
+        int[] testArray_1 = new int[]{1, 2};
+        int[] testArray_2 = new int[]{3};
+
+        //When
+        double actualValue = new Solution().findMedianSortedArrays(testArray_1, testArray_2);
+
+        //Then
+        assertEquals(2.0, actualValue, 0.00001);
+    }
+
+    @Test
+    public void findMedianSortedArraysTest_2() {
+        //Given
+        int[] testArray_1 = new int[]{1, 2};
+        int[] testArray_2 = new int[]{3, 4};
+
+        //When
+        double actualValue = new Solution().findMedianSortedArrays(testArray_1, testArray_2);
+
+        //Then
+        assertEquals(2.5, actualValue, 0.00001);
+    }
+
+    @Test
+    public void findMedianSortedArraysTest_3() {
+        //Given
+        int[] testArray_1 = new int[]{};
+        int[] testArray_2 = new int[]{};
+
+        //When
+        double actualValue = new Solution().findMedianSortedArrays(testArray_1, testArray_2);
+
+        //Then
+        assertEquals(0, actualValue, 0.00001);
+    }
+
+    @Test
+    public void findMedianSortedArraysTest_4() {
+        //Given
+        int[] testArray_1 = new int[]{1,2,3};
+        int[] testArray_2 = new int[]{};
+
+        //When
+        double actualValue = new Solution().findMedianSortedArrays(testArray_1, testArray_2);
+
+        //Then
+        assertEquals(2, actualValue, 0.00001);
+    }
+
+    @Test
+    public void findMedianSortedArraysTest_5() {
+        //Given
+        int[] testArray_1 = new int[]{};
+        int[] testArray_2 = new int[]{1,2,3};
+
+        //When
+        double actualValue = new Solution().findMedianSortedArrays(testArray_1, testArray_2);
+
+        //Then
+        assertEquals(2, actualValue, 0.00001);
+    }
+
 
 }
