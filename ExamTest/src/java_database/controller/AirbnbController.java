7860635f2aad6332f6509ce0861dd7e6e91a7980
ZsoltWebler <128636694+ZsoltWebler@users.noreply.github.com>
2024-11-28 17:31:06 +0100

    Init

--- ExamTest/src/java_database/controller/AirbnbController.java
+++ ExamTest/src/java_database/controller/AirbnbController.java
@@ -1,12 +1,16 @@
 package java_database.controller;
 
 import java_database.repository.ApartmentRepository;
+import java_database.view.Airbnb;
 
 public class AirbnbController {
 
 
     public AirbnbController(){
+        ApartmentRepository apartmentRepository = new ApartmentRepository();
 
+        Airbnb airbnb = new Airbnb(apartmentRepository);
+        airbnb.setVisible(true);
     }
 
 
