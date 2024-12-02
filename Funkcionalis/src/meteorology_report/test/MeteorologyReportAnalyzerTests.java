a8cee0352b7e442bd906ce2b94b38ecb450f150c
ZsoltWebler <128636694+ZsoltWebler@users.noreply.github.com>
2024-12-02 18:55:53 +0100

    Init

--- Funkcionalis/src/meteorology_report/test/MeteorologyReportAnalyzerTests.java
+++ Funkcionalis/src/meteorology_report/test/MeteorologyReportAnalyzerTests.java
@@ -1,13 +1,19 @@
 package meteorology_report.test;
 
+import meteorology_report.MeteorologyReport;
+import meteorology_report.MeteorologyReportAnalyzer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class MeteorologyReportAnalyzerTests {
 
 
     @Test
     public void lastReportFromCityTest(){
+       // new MeteorologyReportAnalyzer(List.of(new MeteorologyReport()))
+
         Assertions.fail();
     }
 
